13acee1238cd4dce2afb6b895194681076860829
chen hui run <chenhr@lxslc705.ihep.ac.cn>
2021-01-25 14:15:45 +0800

    init

---
+++ datafill.C
@@ -0,0 +1,166 @@
+#include "utils.h"
+
+void datafill()
+{
+
+  char *cf_cats = (char*)"cats.cfg";
+  char *cf_bins = (char*)"binnings.cfg";
+  map<TString, string> catCuts;
+  map<TString, vector<float>> catBins;
+  getCatCuts(cf_cats, catCuts);
+  getCatBinning(cf_bins, catBins);
+
+
+  map<TString, pair<float, float>> bins;
+////  bins[""] = make_pair(-999999999, 999999999);
+//  bins["b1"] = make_pair(-999999999, -3);
+//  bins["b2"] = make_pair(-3, -1.5);
+//  bins["b3"] = make_pair(-1.5, -1);
+//  bins["b4"] = make_pair(-1, -0.5);
+//  bins["b5"] = make_pair(-0.5, 0);
+//  bins["b6"] = make_pair(0, 0.5);
+//  bins["b7"] = make_pair(0.5, 1);
+//  bins["b8"] = make_pair(1, 1.5);
+//  bins["b9"] = make_pair(1.5, 3);
+//  bins["b10"] = make_pair(3, 99999999);
+//  bins["b1"] = make_pair(-999999999, -2);
+//  bins["b2"] = make_pair(-2, -1);
+//  bins["b3"] = make_pair(-1, 0);
+//  bins["b4"] = make_pair(0, 1);
+//  bins["b5"] = make_pair(1, 2);
+//  bins["b6"] = make_pair(2, 99999999);
+  bins["b1"] = make_pair(-999999999, -2);
+  bins["b2"] = make_pair(-2, -1);
+  bins["b3"] = make_pair(-1, 0);
+  bins["b4"] = make_pair(0, 1);
+  bins["b5"] = make_pair(1, 2);
+  bins["b6"] = make_pair(2, 99999999);
+
+  std::map<TString, std::vector<float>> cats;
+  cats["TT"] = {0.14, 1., 0.23, 1.};
+  cats["TL"] = {0.14, 1., -1., 0.23};
+  cats["LT"] = {-1, 0.14, 0.05, 1.};
+  cats["LL"] = {-1, 0.14, -1., 0.05};
+
+  //TString dirpath = "/scratchfs/bes/chenhr/atlaswork/VBF_CP/ntuples/";
+  //TString dirpath = "/workfs/bes/chenhr/atlas/vbf_cp/ntuples/";//
+  TString dirpath = "/scratchfs/bes/chenhr/atlaswork/VBF_CP/h025/";
+  std::string path_str = dirpath.Data();
+  std::vector<std::string> sub_dirs = getDirBinsSortedPath(path_str);
+
+  std::vector<std::string> files(0);
+
+  for(auto d : sub_dirs){
+    if(d==".") continue;
+    if(d.find("data") == std::string::npos) continue;
+    if(d.find(".root") != std::string::npos) continue;
+    cout<<"d: "<<path_str+d<<endl;
+    std::vector<std::string> fs = getDirBinsSortedPath(path_str+d+"/");
+    for(auto f : fs){
+      if(f==".") continue;
+      if(f.find("slim") != std::string::npos) continue;
+      cout<<"f: "<<path_str+"/"+d+"/"+f<<endl;
+      files.push_back(path_str+d+"/"+f);
+      
+    }
+  }
+
+  cout<<"\n"<<endl;
+
+  TString sample = "dataSB";
+//  TString sample = "fulldata";
+
+  cout<<"processing "<<sample<<".."<<endl;
+
+  Int_t N_j,N_photon,cutflow,Category;
+  Float_t m_yy,pT_y1,pT_y2,m_jj_30,DeltaEta_jj,Zepp,oo1,oo2,WeightDtilde1,WeightDtilde2,weight,xsec_kF_eff,total_weights;
+  Bool_t isDalitz,isPassed,isPassedIsolation,isPassedPID,isPassedTriggerMatch;
+
+  for(auto bin = bins.begin(); bin != bins.end(); bin++){
+
+    cout<<bin->first<<endl;
+
+    double b_l = bin->second.first;
+    double b_r = bin->second.second;
+
+    TH1F *h_myy = new TH1F("m_yy_"+sample, "", 110, 105, 160);
+    TH1F *h_oo = new TH1F("oo_"+sample, "", 40, -20, 20);
+  
+    TFile *f_out = new TFile("hists_"+sample+"_"+bin->first+".root","recreate");
+
+    TTree *t_out = new TTree("CollectionTree", "");
+
+    double myy_out, oo_out, weight_out;
+    t_out->Branch("m_yy", &myy_out, "m_yy/D");
+    t_out->Branch("oo1", &oo_out, "oo1/D");
+    t_out->Branch("weight", &weight_out, "weight/D");
+  
+    for(auto f : files){
+      TString filepath = f.data();
+      TFile *f_in = new TFile(filepath,"read");
+      TTree *tree = (TTree*) f_in->Get("output");
+  
+      cout<<filepath<<endl;
+  
+  
+      tree->SetBranchAddress("cutflow", &cutflow);
+      //tree->SetBranchAddress("Category", &Category);//
+      tree->SetBranchAddress("catCoup_XGBoost_ttH", &Category);
+      tree->SetBranchAddress("isDalitz", &isDalitz);
+      tree->SetBranchAddress("isPassed", &isPassed);
+      tree->SetBranchAddress("isPassedIsolation", &isPassedIsolation);
+      tree->SetBranchAddress("isPassedPID", &isPassedPID);
+      tree->SetBranchAddress("isPassedTriggerMatch", &isPassedTriggerMatch);
+      tree->SetBranchAddress("m_yy", &m_yy);
+      //tree->SetBranchAddress("N_j", &N_j);//
+      tree->SetBranchAddress("N_j_30", &N_j);
+      tree->SetBranchAddress("N_photon", &N_photon);
+      tree->SetBranchAddress("pT_y1", &pT_y1);
+      tree->SetBranchAddress("pT_y2", &pT_y2);
+      tree->SetBranchAddress("m_jj_30", &m_jj_30);
+      tree->SetBranchAddress("DeltaEta_jj", &DeltaEta_jj);
+      tree->SetBranchAddress("Zepp", &Zepp);
+      tree->SetBranchAddress("oo1", &oo1);
+      tree->SetBranchAddress("oo2", &oo2);
+  
+  
+      Long64_t endentry = tree->GetEntries();
+  
+      for(int i = 0; i < endentry; i++){
+        tree->GetEntry(i);
+        if(i%100000==0&&i!=0) std::cout<<i<<" events processed"<<std::endl;
+        if(i==endentry-1) cout<<endentry<<" events processed"<<endl;
+        if(isDalitz==1) continue;
+        if(isPassed==0) continue;
+        if(N_j<2) continue;
+        if(m_jj_30/1000<400) continue;
+        if(DeltaEta_jj>-2&&DeltaEta_jj<2) continue;
+        if(Zepp>5||Zepp<-5) continue;
+        if(Category<11||Category>14) continue;
+  
+        if(m_yy/1000>120&&m_yy/1000<130) continue;
+
+        if(oo1<b_l||oo1>=b_r) continue; 
+
+        //myy_out = m_yy/1000;
+        myy_out = m_yy;
+        oo_out = oo1;
+        weight_out = 1.;
+        t_out->Fill();
+ 
+        h_myy->Fill(m_yy/1000);
+        h_oo->Fill(oo1);
+  
+      }
+      delete f_in;
+    }
+    f_out->cd();
+    t_out->Write();
+    h_myy->Write();
+    h_oo->Write();
+    //f_out->Close();
+  
+    delete h_myy;
+    delete h_oo;
+  }
+}
---
+++ utils.h
@@ -0,0 +1,127 @@
+#include <iterator>
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <dirent.h>
+
+#include <sstream>
+#include <fstream>
+
+#include <string.h>
+
+using namespace std;
+
+static bool readConfigFile(const char * cfgfilepath, const string & key, string & value)
+{
+    fstream cfgFile;
+    cfgFile.open(cfgfilepath);
+    if( ! cfgFile.is_open())
+    {
+        cout<<"can not open cfg file!"<<endl;
+        return false;
+    }
+    char tmp[1000];
+    while(!cfgFile.eof())
+    {
+        cfgFile.getline(tmp,1000);
+        string line(tmp);
+        size_t pos = line.find(':');
+        if(pos==string::npos) continue;
+        string tmpKey = line.substr(0,pos);
+        if(key==tmpKey)
+        {
+            value = line.substr(pos+1);
+        }
+    }
+    return false;
+}
+
+
+int fileNameFilter(const struct dirent *cur) {
+    std::string str(cur->d_name);
+    if (str.find("..") != std::string::npos) {
+        return 0;
+    }
+    return 1;
+}
+
+std::vector<std::string> getDirBinsSortedPath(std::string dirPath) {
+    struct dirent **namelist;
+    std::vector<std::string> ret;
+    int n = scandir(dirPath.c_str(), &namelist, fileNameFilter, alphasort);
+    if (n < 0) {
+        return ret;
+    }
+    for (int i = 0; i < n; ++i) {
+        std::string filePath(namelist[i]->d_name);
+        ret.push_back(filePath);
+        free(namelist[i]);
+    };
+    free(namelist);
+    return ret;
+}
+
+vector<string> readInLines(const char * cfgfilepath){
+    vector<string> readin;
+
+    fstream cfgFile;
+    cfgFile.open(cfgfilepath);
+    if( ! cfgFile.is_open())
+    {
+        cout<<"can not open cfg file!"<<endl;
+    }
+    char tmp[1000];
+    while(!cfgFile.eof())
+    {
+        cfgFile.getline(tmp,1000);
+        string line(tmp);
+        readin.push_back(line);
+    }
+
+    return readin;
+}
+
+map<TString, string> sepKeyValue(string cfg){
+  map<TString, string> cats;
+
+  vector<string> lines = readInLines(cfg.data());
+  for(auto l : lines){
+    int pos = l.find(":");
+    string sKey = l.substr(0,pos);
+    string sValues = l.substr(pos+1);
+    TString tsKey = sKey.data();
+    tsKey.ReplaceAll(" ", "");
+
+    cats[tsKey] = sValues;
+  }
+
+  return cats;
+}
+
+void getCatCuts(string cfg, map<TString, string> &catCuts){
+  catCuts = sepKeyValue(cfg);
+}
+
+void getCatBinning(string cfg, map<TString, vector<float>> &catBins){
+  map<TString, string> cats = sepKeyValue(cfg);
+
+  for(auto c : cats){
+    string sBinEdges = c.second;
+    TString tsCat = c.first;
+ 
+    TString ts_tmp;
+    vector<float> f_vec;
+    char *p;
+    char *buff = (char*) sBinEdges.data();
+    char *sep = (char*)",";
+    p = strsep(&buff, sep);
+    while(p!=NULL){
+      ts_tmp = p;
+      ts_tmp.ReplaceAll(" ", "");
+      if(ts_tmp!="") f_vec.push_back((float)atof(ts_tmp.Data())); //cout<<ts_tmp<<endl;
+      p = strsep(&buff, sep);
+    }
+
+    catBins[tsCat] = f_vec;
+  }
+}
